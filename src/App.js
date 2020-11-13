b34db3c0e4032c30f56d3b1d58873c79826f117e
naito_takumi <naito_takumi@takenaka-co.co.jp>
2020-11-13 19:09:00 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@
 //     dom
 //   );
 // }
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 15},
-    {name: "Jiro"},
+    {name: "Jiro", age: 12},
   ]
   return (
     <div>
@@ -27,7 +28,8 @@ const User = (props) => {
   return <div>Hi! My name is {props.name}, and {props.age} years old.</div>
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 export default App;
