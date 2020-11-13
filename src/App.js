e51e2ed9180b66d0fa4cbb2dd3b30da2e03b4611
naito_takumi <naito_takumi@takenaka-co.co.jp>
2020-11-13 18:48:27 +0900

    create components and functional components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  const greeting = "Hi!"
-  const dom = <h1>{greeting}</h1>;
+// function App() {
+//   const greeting = "Hi!"
+//   const dom = <h1>{greeting}</h1>;
+//   return (
+//     dom
+//   );
+// }
+
+const App = () => {
   return (
-    dom
-  );
+    <div>
+      <Cat></Cat>
+      <Cat></Cat>
+      <Cat></Cat>
+    </div>
+  )
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
