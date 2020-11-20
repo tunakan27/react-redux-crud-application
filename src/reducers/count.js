dd83b88bd5d8ca756bcf4a0f462b982e035777e2
tunakan27 <takumi.naito27@gmail.com>
2020-11-20 20:03:40 +0900

    fix typo for INCREMENT

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,10 +1,10 @@
-import { INCREMNET, DECREMENT } from '../actions';
+import { INCREMENT, DECREMENT } from '../actions';
 
 const initialState = { value: 0 }
 
 export default (state = initialState, action) => {
   switch(action.type) {
-    case INCREMNET:
+    case INCREMENT:
       return { value: state.value + 1 }
     case DECREMENT:
       return {value: state.value - 1 }
